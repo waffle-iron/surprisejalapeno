cbd700938f439f532f641f604cb1bbecfacb542a
D. Wren <drawnwren@gmail.com>
2016-09-05 13:34:16 -0700

    Two easy tests

--- test/test.js
+++ test/test.js
@@ -1,16 +1,15 @@
 var expect = require('chai').expect;
 var path = require('path');
-var supertest = require('supertest');
 
 describe('Tooling', function() {
 
   describe('Tests', function () {
     it('should pass truthy tests', function () {
-        .expect(true).to.be.truthy();
+        expect(true).to.be.true;
     });
 
-    it('should fail failing tests', function (done) {
-        .expect(false).to.not.be.truthy();
+    it('should fail failing tests', function () {
+        expect(false).to.not.be.true;
     });
   });
 
