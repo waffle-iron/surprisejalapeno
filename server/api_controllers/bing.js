3f593fc125c3841e6086e1cecabd8b3706ffdb27
D. Wren <drawnwren@gmail.com>
2016-09-07 14:40:33 -0700

    Changed to use only env variables so Travis will stop bitching.

--- server/api_controllers/bing.js
+++ server/api_controllers/bing.js
@@ -1,14 +1,12 @@
 const helpers = require('../api_helpers/general');
 
-const creds = require('../config/creds');
-
 const baseUrl = 'https://api.cognitive.microsoft.com/bing/v5.0/news/search';
 
 function search(query) {
   let escapedQuery = query.split(' ').join('+');
   escapedQuery = encodeURIComponent(escapedQuery);
   const qUrl = `${baseUrl}?q=${escapedQuery}`;
-  return helpers.getUrl(qUrl, creds.bing || { 'Ocp-Apim-Subscription-Key': process.env.bing });
+  return helpers.getUrl(qUrl, { 'Ocp-Apim-Subscription-Key': process.env.bing });
 }
 
 function searchHandler(req, res, next) {
