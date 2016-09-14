931f8ac28629c8930b9fbc0c8a0c43f0b45cb7f9
Daniel Snyder <daniel.c.snyder@gmail.com>
2016-09-14 15:36:57 -0700

    Update query to Alchemy to single out city from user input

--- server/api_helpers/sherlock.js
+++ server/api_helpers/sherlock.js
@@ -4,28 +4,29 @@ const qs = require('querystring');
 
 // given a placename, returns all news articles with that entity in the title
 // and body
-function getByPlace(place) {
+const getByPlace = (place) => {
   console.log('getByPlace sherlock.js query builder input: ', place);
   // Base Alchemy News API endpoint
   let qUrl = 'https://gateway-a.watsonplatform.net/calls/data/GetNews';
   // Configure all of the individual queries for querying the endpoint
   // See the alchemy news api for what options are available
+  const API_KEY = null;
   let queries = {
     outputMode: 'json',
     start: 'now-1d',
     end: 'now',
-    count: 50,
+    count: 5,
     return: 'enriched.url.title,enriched.url.text,'
       + 'enriched.url.url,enriched.url.entities,'
       + 'enriched.url.publicationDate.date',
-    apikey: process.env.alchemy
+    apikey: API_KEY
   };
   // Turn all of the queries into a query string
   queries = qs.stringify(queries);
   // Append the queries to the base url
   qUrl = `${qUrl}?${queries}`;
   // Append the entity search to the base url.
-  qUrl += `&q.enriched.url.entities.entity=|text=${place}|`;
+  qUrl += `&q.enriched.url.entities.entity=|text=${place},type=city|`;
   // return a promise from the helpers geturl function
   return helpers.getUrl(qUrl).then(d => {
     const resp = JSON.parse(d);
@@ -36,6 +37,6 @@ function getByPlace(place) {
     console.log('return helpers.getUrl resp.result: ', resp);
     return resp.result;
   });
-}
+};
 
 exports.getByPlace = getByPlace;
